a6986528f65db9cd6e1cb306ed8c5455df9fda05
victor2199 <victorwilliams2199@gmail.com>
2025-07-25 09:17:19 +0100

    Final capstone submission

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,11 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   eslint: {
+    // ✅ THIS disables all ESLint checks during build
     ignoreDuringBuilds: true,
+  },
+  images: {
+    unoptimized: true, // Optional: disables image optimization if needed
   },
 };
 
